de72e88343542f8a7325bf6c4090ebd3c04f3927
andresperez0401 <74725819+andresperez0401@users.noreply.github.com>
2023-06-12 14:11:15 -0400

    Update

--- src/Usuario/Dominio/usuario.ts
+++ src/Usuario/Dominio/usuario.ts
@@ -1,9 +1,14 @@
-import { nombreUsuario } from "./nombreUsuario-VO";
-import { idUsuario } from "./idUsuario_VO";
-import { apellidoUsuario } from "./apellidoUsuario-VO";
+import { apellidoUsuario } from './value_objects/apellidoUsuario';
+import { claveUsuario } from './value_objects/claveUsuario';
+import { emailUsuario } from './value_objects/emailUsuario';
+import { idUsuario } from './value_objects/idUsuario';
+import { nombreUsuario } from './value_objects/nombreUsuario';
 
-export class usuario {
-    id: idUsuario;
-    nombre: nombreUsuario;
-    apellido: apellidoUsuario;
+export class Usuario {
+  private id?: idUsuario;
+  private nombre: nombreUsuario;
+  private apellido: apellidoUsuario;
+  private email: emailUsuario;
+  private clave: claveUsuario;
+  private suscripcion: boolean;
 }
