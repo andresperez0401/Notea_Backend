93e7db49442e32011500cb42312b5242e7eeb6c8
Sergio Capon Blanco <scapon.17@est.ucab.edu.ve>
2023-06-14 19:04:36 -0400

    Servicio de crear y buscar etiquetas
    Se finalizo la creacion de los servicios que brindan e implementan la funcionalidad de crear una etiqueta y de buscar etiquetas
    Para ello se creo el agregado etiqueta junto con los value objects necesarios y su respectivo repositorio

---
+++ src/Etiqueta/Dominio/AgregadoEtiqueta.ts
@@ -0,0 +1,57 @@
+import { error } from 'console';
+import { colorEtiqueta } from './ValueObjectsEtiqueta/colorEtiqueta';
+import { idEtiqueta } from './ValueObjectsEtiqueta/idEtiqueta';
+import { nombreEtiqueta } from './ValueObjectsEtiqueta/nombreEtiqueta';
+import { Optional } from 'src/Utils/opcional';
+
+export class Etiqueta {
+  private id: idEtiqueta;
+  private nombre: nombreEtiqueta;
+  private color: colorEtiqueta;
+
+  private constructor(
+    nombre: nombreEtiqueta,
+    color: colorEtiqueta,
+    id?: idEtiqueta,
+  ) {
+    this.id = id;
+    this.nombre = nombre;
+    this.color = color;
+  }
+
+  static crearEtiqueta(
+    nombre: string,
+    color: colorEtiqueta,
+    id?: string,
+  ): Etiqueta {
+    return new Etiqueta(
+      nombreEtiqueta.crearNombreEtiqueta(nombre),
+      colorEtiqueta[color],
+      idEtiqueta.crearIdEtiqueta(id),
+    );
+  }
+
+  public getId(): string {
+    return this.id.getValue();
+  }
+
+  public getNombre(): string {
+    return this.nombre.getNombreEtiqueta();
+  }
+
+  public getColor(): string {
+    return this.color.toString();
+  }
+
+  public setColor(color: colorEtiqueta): void {
+    this.color = color;
+  }
+
+  public setNombre(nombre: nombreEtiqueta): void {
+    this.nombre = nombre;
+  }
+
+  public setId(id: idEtiqueta): void {
+    this.id = id;
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './db/db.module';
 import { UsuarioModule } from './Usuario/Infraestructura/usuario.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { NotaModule } from './Nota/Infraestructura/nota.module';
+import { EtiquetaModule } from './Etiqueta/Infraestructura/etiqueta.module';
+
 
 @Module({
-  imports: [DatabaseModule, UsuarioModule],
-  controllers: [],
-  providers: [],
+  imports: [DatabaseModule, NotaModule, UsuarioModule,EtiquetaModule], //aca debemos importar todos los modulos que vayamos creando
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
