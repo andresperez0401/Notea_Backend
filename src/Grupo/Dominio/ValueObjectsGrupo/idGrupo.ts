0dde4c9ec78afc35ea4356a78a35a48cd06d0a35
andresperez0401 <74725819+andresperez0401@users.noreply.github.com>
2023-06-19 14:39:55 -0400

    Elementos del dominio de Grupo

---
+++ src/Grupo/Dominio/ValueObjectsGrupo/idGrupo.ts
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+import { Optional } from 'src/Utils/opcional';
+import { ValueObject } from 'src/core/domain/value_objects/ValueObject';
+import { v4 as uuidv4 } from 'uuid';
+
+export class idGrupo implements ValueObject<string> {
+  private readonly id: string;
+
+  private constructor(id: Optional<string>) {
+    if (id.hasvalue()){
+      this.id = id.getValue();
+    }
+    else{
+      this.id = uuidv4();
+    }
+  }
+
+  static crearIdGrupo(id?: string): idGrupo {
+    return new idGrupo(new Optional<string>(id));
+  }
+
+  isValid(): boolean {
+    return this.id.length > 0;
+  }
+
+  equals(vo: this): boolean {
+    return this.id === vo.id;
+  }
+
+  getValue(): string {
+    return this.id;
+  }
+}
