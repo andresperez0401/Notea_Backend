440b2d8079271844aa9e85cb85c0638df770ddda
Delta27222 <angelgabriel.hernandez@gmail.com>
2023-06-20 09:12:57 -0400

    Error en commit

--- src/Usuario/Dominio/usuario.ts
+++ src/Usuario/Dominio/usuario.ts
@@ -4,6 +4,7 @@ import { emailUsuario } from './value_objects/emailUsuario';
 import { idUsuario } from './value_objects/idUsuario';
 import { nombreUsuario } from './value_objects/nombreUsuario';
 
+
 export class Usuario {
   private id?: idUsuario;
   private nombre: nombreUsuario;
@@ -12,43 +13,38 @@ export class Usuario {
   private clave: claveUsuario;
   private suscripcion: boolean;
 
-  private constructor(
-    nombre: nombreUsuario,
-    apellido: apellidoUsuario,
-    email: emailUsuario,
-    clave: claveUsuario,
-    suscripcion: boolean,
-    id?: idUsuario,
-  ) {
-    if (id !== undefined) {
-      this.id = id;
-    } else {
+  private constructor(nombre: nombreUsuario, apellido: apellidoUsuario, email:emailUsuario,
+    clave: claveUsuario, suscripcion: boolean, id?:idUsuario){
+     if (id !== undefined){
+      this.id = id
+     }
+     else{
       this.id = new idUsuario();
-    }
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.email = email;
-    this.clave = clave;
-    this.suscripcion = suscripcion;
+     }
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.email = email;
+      this.clave = clave;
+      this.suscripcion = suscripcion;
   }
 
-  static crearUsuario(
-    nombre: nombreUsuario,
-    apellido: apellidoUsuario,
-    email: emailUsuario,
-    clave: claveUsuario,
-    suscripcion: boolean,
-    id?: idUsuario,
-  ) {
-    return new Usuario(nombre, apellido, email, clave, suscripcion);
+  static crearUsuario(nombre: nombreUsuario, apellido: apellidoUsuario, email:emailUsuario,
+    clave: claveUsuario, suscripcion: boolean, id? : idUsuario){
+    
+      return new Usuario(
+        nombre,
+        apellido,
+        email,
+        clave,
+        suscripcion
+      )
   }
 
-  public getId(): string {
-    //getters del usuario
+  public getId(): string {          //getters del usuario 
     return this.id.getValue();
   }
-
-  public getNombre(): string {
+  
+  public getNombre():string {
     return this.nombre.getValue();
   }
 
@@ -56,15 +52,15 @@ export class Usuario {
     return this.apellido.getValue();
   }
 
-  public getEmail(): string {
+  public getEmail() : string {
     return this.email.getValue();
   }
 
-  public isSuscribed(): boolean {
+  public isSuscribed() : boolean {
     return this.suscripcion;
   }
 
-  public getClave(): string {
+  public getClave() : string {
     return this.clave.getValue();
   }
 }
