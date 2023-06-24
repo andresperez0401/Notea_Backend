5d21e40c651f0f56a3b14bc495232e09c4425ae2
Sergio Capon Blanco <129621868+SerCap98@users.noreply.github.com>
2023-06-24 00:21:42 -0400

    Update CrearUsuario.service.ts

--- src/Usuario/Aplicacion/CrearUsuario.service.ts
+++ src/Usuario/Aplicacion/CrearUsuario.service.ts
@@ -1,37 +1,37 @@
 import { IAplicationService } from 'src/core/domain/appService/IAplicationService';
-import { Injectable, Inject } from '@nestjs/common';
-import { Either } from 'src/utils/either';
+import { Either } from 'src/Utils/Either';
 import { CrearUsuarioDto } from './dto/CrearUsuario.dto';
 import { Usuario } from '../Dominio/AgregadoUsuario';
 import { RepositorioUsuario } from '../Dominio/RepositorioUsuario';
-import { nombreUsuario } from '../Dominio/value_objects/nombreUsuario';
-import { apellidoUsuario } from '../Dominio/value_objects/apellidoUsuario';
-import { emailUsuario } from '../Dominio/value_objects/emailUsuario';
-import { claveUsuario } from '../Dominio/value_objects/claveUsuario';
-import { RepositorioUsuarioImp } from '../Infraestructura/repository/RepositorioUsuarioImp';
+import { EventPublisher } from 'src/core/domain/events/EventPublisher';
+import { UsuarioCreadoEvent } from '../Dominio/eventos/UsuarioCreadoEvent'; // Importa el evento
 
-@Injectable()
-export class CrearUsuarioService
-  implements IAplicationService<CrearUsuarioDto, Usuario>
-{
+export class CrearUsuarioService implements IAplicationService<CrearUsuarioDto, Usuario> {
   private readonly repositorioUsuario: RepositorioUsuario;
+  private readonly eventPublisher: EventPublisher;
 
-  constructor(
-    @Inject(RepositorioUsuarioImp)
-    repositorioUsuario: RepositorioUsuario,
-  ) {
+  constructor(repositorioUsuario: RepositorioUsuario, eventPublisher: EventPublisher) {
     this.repositorioUsuario = repositorioUsuario;
+    this.eventPublisher = eventPublisher;
   }
 
   async execute(s: CrearUsuarioDto): Promise<Either<Usuario, Error>> {
     const newUser: Usuario = Usuario.crearUsuario(
-      new nombreUsuario(s.nombre),
-      new apellidoUsuario(s.apellido),
-      new emailUsuario(s.email),
-      new claveUsuario(s.clave),
-      s.suscripcion,
+      s.nombre,
+      s.apellido,
+      s.email,
+      s.clave,
+      s.suscripcion
     );
 
-    return await this.repositorioUsuario.crearUsuario(newUser);
+    const resultado = await this.repositorioUsuario.crearUsuario(newUser);
+ 
+    if (resultado.isLeft()) {
+
+      // Publica el evento UsuarioCreadoEvent
+      this.eventPublisher.publish(newUser);
+    }
+
+    return resultado;
   }
 }
