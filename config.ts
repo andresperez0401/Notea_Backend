d8c119e760dfec47e12f004dbf60f458601ceff1
JosMeeh <josen1401@hotmail.com>
2023-07-02 22:00:46 -0400

    Agregada la funcionalidad al nuevo contenedor de nota en el boton  de "IMAGEN A TEXTO"

--- config.ts
+++ config.ts
@@ -9,5 +9,5 @@
 export const DB_HOST1 = process.env.DB_HOST || 'localhost';
 export const DB_PORT1 = process.env.DB_PORT || '5433';
 export const DB_USER1 = process.env.DB_USER || 'postgres';
-export const DB_PASSWORD1 = process.env.DB_PASSWORD || 'perezdasilva';
-export const DB_NAME1 = process.env.DB_NAME || 'New_Notea_App_DB';
+export const DB_PASSWORD1 = process.env.DB_PASSWORD || 'jn5023';
+export const DB_NAME1 = process.env.DB_NAME || 'New_Note_App_DB';
