50cae69affaca07066977f11fde0d911253ef836
Italo Visconti <hermanosvisconti@hotmail.com>
2023-07-17 23:59:28 -0400

    bug al modificar etiquetas arreglado

--- src/Nota/Aplicacion/ModificarNota.service.ts
+++ src/Nota/Aplicacion/ModificarNota.service.ts
@@ -1,61 +1,62 @@
-import { IAplicationService } from "src/core/domain/appService/IAplicationService";
-import { Nota } from "../Dominio/AgregadoNota";
-import { Either } from "src/Utils/Either";
-import { RepositorioNota } from "../Dominio/RepositorioNota";
-import { ModificarNotaDto } from "./dto/ModificarNota.dto";
-import { VOImagen } from "../Dominio/Entidades/VOImagen";
-import { EstadoEnum } from "../Dominio/ValueObjectsNota/EstadoEnum";
-import { Optional } from "src/Utils/Opcional";
+import { IAplicationService } from 'src/core/domain/appService/IAplicationService';
+import { Nota } from '../Dominio/AgregadoNota';
+import { Either } from 'src/Utils/Either';
+import { RepositorioNota } from '../Dominio/RepositorioNota';
+import { ModificarNotaDto } from './dto/ModificarNota.dto';
+import { VOImagen } from '../Dominio/Entidades/VOImagen';
+import { EstadoEnum } from '../Dominio/ValueObjectsNota/EstadoEnum';
+import { Optional } from 'src/Utils/Opcional';
 
+export class ModificarNotaService
+  implements IAplicationService<ModificarNotaDto, string>
+{
+  private readonly repositorioNota: RepositorioNota;
 
-export class ModificarNotaService implements IAplicationService<ModificarNotaDto,string>{
+  constructor(repositorioNota: RepositorioNota) {
+    this.repositorioNota = repositorioNota;
+  }
 
-    private readonly repositorioNota: RepositorioNota;
+  async execute(s: ModificarNotaDto): Promise<Either<string, Error>> {
+    const estado = EstadoEnum.GUARDADO;
 
-    constructor (
-        repositorioNota: RepositorioNota){
-        
-        this.repositorioNota = repositorioNota;
+    let im;
+
+    let contenido = new Optional<any>();
+    if (s.contenido) {
+      const auxcontenido = JSON.stringify(s.contenido);
+      contenido = new Optional<any>(JSON.parse(auxcontenido));
+      //si hacemos un multipart el contenido se mandan como un string y hay que parsearlo
+      //a pesar de ser inestable, es la unica forma de mandar un array de objetos distintos
+      //pido disculpas
     }
 
-    async execute(s: ModificarNotaDto): Promise<Either<string,Error>> {      
-        
-      const estado = EstadoEnum.GUARDADO;
-
-      let im;
-
-      let contenido = new Optional<any>();
-      if (s.contenido) {
-        const auxcontenido = JSON.stringify(s.contenido);
-        contenido = new Optional<any>(JSON.parse(auxcontenido));
-        //si hacemos un multipart el contenido se mandan como un string y hay que parsearlo
-        //a pesar de ser inestable, es la unica forma de mandar un array de objetos distintos
-        //pido disculpas
-      }
-
-      if (s.imagenes) {
-        im = s.imagenes.map((i) => {
-          return VOImagen.crearImagenNota(i.nombre, i.buffer);
-        });
-      }
-
-      const opLatitud = new Optional<number>(s.latitud);
-      const opLongitud = new Optional<number>(s.longitud);
-      const opEtiquetas = new Optional<string[]>(s.etiquetas);
-
-
-      const nota = Nota.crearNota( //factory agregado
-        s.titulo,
-        s.fechaCreacion,
-        estado,
-        s.grupo,
-        opLatitud,
-        opLongitud,
-        contenido,
-        opEtiquetas,
-        s.id,
-      );
-
-        return await this.repositorioNota.updateNota(nota);
+    if (s.imagenes) {
+      im = s.imagenes.map((i) => {
+        return VOImagen.crearImagenNota(i.nombre, i.buffer);
+      });
     }
+
+    const opLatitud = new Optional<number>(s.latitud);
+    const opLongitud = new Optional<number>(s.longitud);
+
+    let opEtiquetas = new Optional<string[]>([]);
+    if (s.etiquetas) {
+      opEtiquetas = new Optional<string[]>(s.etiquetas);
+    }
+
+    const nota = Nota.crearNota(
+      //factory agregado
+      s.titulo,
+      s.fechaCreacion,
+      estado,
+      s.grupo,
+      opLatitud,
+      opLongitud,
+      contenido,
+      opEtiquetas,
+      s.id,
+    );
+
+    return await this.repositorioNota.updateNota(nota);
+  }
 }
