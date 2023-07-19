202c4bac6313686c1db74f39269f167fc7860ce6
andresperez0401 <74725819+andresperez0401@users.noreply.github.com>
2023-07-19 10:28:55 -0400

    Update crearGrupoMotherObject.ts
    Cuando se prueba un servicio de aplicacion por ejemplo, no hay que llamar al repositorio y validar lo que realize, eso es responsabilidad del test que pruebe algun metodo del repositorio; en este caso dbemos simular el comportamiento del repositorio para el uso del servicio de aplicacion; y luego validar i la respuesta es left, no acoplarnos a detalles de implementacion

--- test/test/ObjectMotherTest/Grupo/crearGrupoMotherObject.ts
+++ test/test/ObjectMotherTest/Grupo/crearGrupoMotherObject.ts
@@ -2,6 +2,9 @@ import { Inject } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { CrearGrupoService } from "src/Grupo/Aplicacion/crearGrupoService";
 import { CrearGrupoDto } from "src/Grupo/Aplicacion/dto/CrearGrupo.dto";
+import { EditarGrupoDto } from "src/Grupo/Aplicacion/dto/EditarGrupo.dto";
+import { GrupoAPapeleraDto } from "src/Grupo/Aplicacion/dto/GrupoAPapelera.dto";
+import { Grupo } from "src/Grupo/Dominio/AgregadoGrupo";
 import { RepositorioGrupo } from "src/Grupo/Dominio/RepositorioGrupo";
 import { EntidadGrupo } from "src/Grupo/Infraestructura/entities/EntidadGrupo";
 import { RepositorioGrupoImp } from "src/Grupo/Infraestructura/repository/RepositorioGrupoImpl";
@@ -12,22 +15,7 @@ import { Repository } from "typeorm";
 
 
 export class CrearGrupoPrueba {
-    @Inject(RepositorioGrupoImp)
-    static repositorioGrupo: RepositorioGrupo;
-
-    
-
-    constructor(
-        @InjectRepository(EntidadGrupo)
-        private readonly grupoRepo: Repository<EntidadGrupo>,
-        @InjectRepository(EntidadNota)
-        private readonly notaRepo: Repository<EntidadNota>,
-        @InjectRepository(EntidadUsuario)
-        private readonly repoUsuario: Repository<EntidadUsuario>,
-      ) {
-        CrearGrupoPrueba.repositorioGrupo = new RepositorioGrupoImp(this.grupoRepo, this.notaRepo, this.repoUsuario);
-      }
-    public static crearGrupoDtoValido(): CrearGrupoDto {
+   public static crearGrupoDtoValido(): CrearGrupoDto {
         const dto = new CrearGrupoDto();
         dto.nombre = 'Test';
         dto.idUsuario = 'c87eb4cb-0d04-49de-8aec-df4abe9c345b';
@@ -41,6 +29,36 @@ export class CrearGrupoPrueba {
     }
     public static crearGrupoService(): CrearGrupoService {
  
-        return new CrearGrupoService(this.repositorioGrupo);
+        const repo : repositorioGrupoPrueba = new repositorioGrupoPrueba();
+        return new CrearGrupoService(repo);
+    }
+}
+
+export class repositorioGrupoPrueba implements RepositorioGrupo{
+    async creargrupo(grupo: Grupo): Promise<Either<Grupo, Error>> {
+        if(grupo.getIdUsuario() == "c87eb4cb-0d04-49de-8aec-df4abe9c345b"){
+            return Either.makeLeft<Grupo, Error>(grupo);
+        }
+        else{
+            return Either.makeRight<Grupo, Error>(new Error("Usuario no encontrado"));
+        }
+    }
+    editarGrupo(grupo: EditarGrupoDto): Promise<Either<Grupo, Error>> {
+        throw new Error("Method not implemented.");
+    }
+    eliminarGrupo(id: string): Promise<Either<string, Error>> {
+        throw new Error("Method not implemented.");
+    }
+    buscarGrupos(): Promise<Either<Iterable<Grupo>, Error>> {
+        throw new Error("Method not implemented.");
+    }
+    buscarGrupoPorId(id: string): Promise<Either<Grupo, Error>> {
+        throw new Error("Method not implemented.");
+    }
+    grupoAPapelera(grupo: GrupoAPapeleraDto): Promise<Either<Grupo, Error>> {
+        throw new Error("Method not implemented.");
+    }
+    buscarGruposDeUsuario(idUsuarioDueno: string): Promise<Either<Iterable<Grupo>, Error>> {
+        throw new Error("Method not implemented.");
     }
 }
